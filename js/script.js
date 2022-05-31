af663bbafb097ed11de623b569224e390b381300
Steven <stevenmancans@gmail.com>
2022-05-31 17:31:57 +1000

    Showing scores in scorecards
    Now showing scores in scorecards. Winning messages now showing on the
    right of the scorecards, need to fix to sit below.

--- js/script.js
+++ js/script.js
@@ -9,26 +9,23 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
 
-    console.log(playerSelection);
-    console.log(computerSelection);
     if (playerSelection == computerSelection) {
-        console.log(`It's a draw`)
+        displayResults.textContent = `It's a draw`
         return `It's a draw!`;
     } else if (playerSelection == "rock" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "rock") {
-        console.log(`${computerSelection} beats ${playerSelection}. You LOSE!!!`);
+        displayResults.textContent = `${computerSelection} beats ${playerSelection}. You LOSE!!!`;
         computerScore += 1;
-        console.log(computerScore)
+        computerResult.textContent = `${computerScore}`;
     } else {
-        console.log(`${playerSelection} beats ${computerSelection}. You WIN!!!`)
+        displayResults.textContent = `${playerSelection} beats ${computerSelection}. You WIN!!!`;
         playerScore += 1;
-        console.log(playerScore);
+        playerResult.textContent = `${playerScore}`;
     }
 }
 
 // Use game() to run playRound() 5 times //
 // If playRound results in a win += to count //
 // After 5 games, determineWinner to show score //
-
 function game() {
     let computerScore = 0;
     let playerScore = 0;
@@ -60,6 +57,18 @@ const btns = document.querySelectorAll('button');
 let computerScore = 0;
 let playerScore = 0;
 
+const results = document.querySelector(".results");
+const displayResults = document.createElement("div");
+results.appendChild(displayResults);
+
+
+const playerResult = document.getElementById("playerScoreWindow");
+playerResult.style.fontSize = "100px";
+playerResult.textContent = `${playerScore}`;
+
+const computerResult = document.getElementById("computerScoreWindow");
+computerResult.style.fontSize = "100px";
+computerResult.textContent = `${computerScore}`;
 
 btns.forEach((button) => {
     button.addEventListener('click', () =>  {
